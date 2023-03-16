7981713c120792733b8cfecad09c81573a845f83
ofcomar <omarff.dil@gmail.com>
2023-03-16 16:18:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
+/*
+ *Prints "Programming is like building a multilingualpuzzle,
+ *
+ *
+ *
+ */
 #include <stdio.h>
+/*
+ *Prints "Programming is like building a multilingual puzzle,
+ *
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
