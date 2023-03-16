6db6b380a4763c7980020de06fef6cefec3d07ec
ofcomar <omarff.dil@gmail.com>
2023-03-16 16:06:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int maini (void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
