41bc96719a5c7e43cd0ffdd4b552fb5ee2a160a5
ofcomar <omarff.dil@gmail.com>
2023-03-16 16:40:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  *
  * main fonction will return 0
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
