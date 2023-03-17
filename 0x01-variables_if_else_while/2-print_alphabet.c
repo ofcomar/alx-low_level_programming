6b5b19ab918f803426b9b66f48ab82aab34ca8a3
ofcomar <omarff.dil@gmail.com>
2023-03-17 16:41:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: 0 good girl 
+ */
+int main (void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	        n++;
+        }
+	putchar('\n');
+	return (0);
+
+}			
