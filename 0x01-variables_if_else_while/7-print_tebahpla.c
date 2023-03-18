75cec29eedc05193b9cef4d311bf0522b549c9eb
ofcomar <omarff.dil@gmail.com>
2023-03-18 15:01:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: 0 good girl
+ */
+int main(void)
+{
+int n = 122;
+
+while (n <= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+
+}
+
