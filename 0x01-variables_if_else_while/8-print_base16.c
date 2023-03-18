a37484d078f6a578acd49c65dde28c17008fdace
ofcomar <omarff.dil@gmail.com>
2023-03-18 15:14:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a <= 57; a++)
+{
+	putchar(a);
+}
+for (a = 97; a <= 102; b++)
+{
+	putchar(b);
+}
+putchar('\n');
+return (0);
+
+
+}
+
