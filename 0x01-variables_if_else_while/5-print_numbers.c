cead86e4a261f06c507f2aa9a4f34893de3a75e2
ofcomar <omarff.dil@gmail.com>
2023-03-20 12:02:46 -0700

    44

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 /**
- * main- all single digit numbers of base 10 starting from 0
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
