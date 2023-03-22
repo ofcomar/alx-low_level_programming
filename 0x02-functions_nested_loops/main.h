853a25b51d7095906f0de9d113ec6cf4529fc4cb
ofcomar <omarff.dil@gmail.com>
2023-03-21 17:26:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
-int _putchar(char c);
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
