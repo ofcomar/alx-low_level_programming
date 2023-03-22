6d1f7a8b37627059ee7e7d064c24c4b2acd914cf
ofcomar <omarff.dil@gmail.com>
2023-03-22 15:51:44 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int)
+{
+    int r;
+
+    print_last_digit(n);
+    r = print_last_digit(n);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
