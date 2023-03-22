349ba4d290e80404816e4bd44cc968cd525763bb
ofcomar <omarff.dil@gmail.com>
2023-03-22 12:18:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,11 @@
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet_x10(void)
-
 {
-        char ch = 'a';
+	char letter;
 
-        while (ch <= 'z')
-        {
-                _putchar(ch);
-                ++ch;
-        }
-        _putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
