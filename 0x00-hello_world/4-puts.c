54a722e186a061fac3775527cfae9df372c0f804
ofcomar <omarff.dil@gmail.com>
2023-03-24 11:02:52 -0700

    0x03-debugging/

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-/*
+/**
  *Prints "Programming is like building a multilingualpuzzle,
  *
  *
  *
  */
 #include <stdio.h>
-/*
+/**
  *Prints "Programming is like building a multilingual puzzle,
  *
- *
+ *main - print exactly "Programming is like building a multilingual puzzle
  */
 int main(void)
 {
