168ed97c54d7f3bd746bd0ce2429da12deba256e
ofcomar <omarff.dil@gmail.com>
2023-04-06 15:18:12 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  * _pow_recursion 
- * integer paramaters is x
- * integer paramater is y 
- * return recursion
+ * x: integer prtrm
+ * y: integer prtrm
+ * Return: recursion
  */
+
 
 int _pow_recursion(int x, int y)
 {
-
 	if (y < 0)
 	{
 		return (-1);
@@ -18,6 +18,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
