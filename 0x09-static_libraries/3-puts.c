a4e46bfdaeb2c90a105339f0190acbb7620686a8
ofcomar <omarff.dil@gmail.com>
2023-04-08 16:38:52 -0700

    0x09

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	for (*str != '\0'; str++)
+	{
+		_puts(*str);
+	}
+	_puts('\n');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute value absolute
+ *
+ * @n: The int to print
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
